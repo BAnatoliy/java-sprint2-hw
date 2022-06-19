f8e97f1e77b02696ae768091b95b462d97a6e364
Anatoliy <bugrov.anatoliy@yandex.ru>
2022-06-19 22:32:24 +1000

    bugs fixed

--- src/InputYearReport.java
+++ src/InputYearReport.java
@@ -13,82 +13,13 @@ public class InputYearReport {
 
     void inputYearlyReport() {
         for (YearlyRecords yearRec : yearRec) {
-            switch (yearRec.month){
-                case 1 : if (yearRec.is_expense) {
-                    expense[0] = yearRec.amount;
-                } else {
-                    gain[0] =yearRec.amount;
-                }
-                    break;
-                case 2 : if (yearRec.is_expense) {
-                    expense[1] = yearRec.amount;
-                } else {
-                    gain[1] = yearRec.amount;
-                }
-                    break;
-                case 3 : if (yearRec.is_expense) {
-                    expense[2] = yearRec.amount;
-                } else {
-                    gain[2] = yearRec.amount;
-                }
-                    break;
-                case 4 : if (yearRec.is_expense) {
-                    expense[3] = yearRec.amount;
-                } else {
-                    gain[3] = yearRec.amount;
-                }
-                    break;
-                case 5 : if (yearRec.is_expense) {
-                    expense[4] = yearRec.amount;
-                } else {
-                    gain[4] = yearRec.amount;
-                }
-                    break;
-                case 6 : if (yearRec.is_expense) {
-                    expense[5] = yearRec.amount;
-                } else {
-                    gain[5] = yearRec.amount;
-                }
-                    break;
-                case 7 : if (yearRec.is_expense) {
-                    expense[6] = yearRec.amount;
-                } else {
-                    gain[6] = yearRec.amount;
-                }
-                    break;
-                case 8 : if (yearRec.is_expense) {
-                    expense[7] = yearRec.amount;
-                } else {
-                    gain[7] = yearRec.amount;
-                }
-                    break;
-                case 9 : if (yearRec.is_expense) {
-                    expense[8] = yearRec.amount;
-                } else {
-                    gain[8] = yearRec.amount;
-                }
-                    break;
-                case 10 : if (yearRec.is_expense) {
-                    expense[9] = yearRec.amount;
-                } else {
-                    gain[9] = yearRec.amount;
-                }
-                    break;
-                case 11 : if (yearRec.is_expense) {
-                    expense[10] = yearRec.amount;
-                } else {
-                    gain[10] = yearRec.amount;
-                }
-                    break;
-                case 12 : if (yearRec.is_expense) {
-                    expense[11] = yearRec.amount;
-                } else {
-                    gain[11] = yearRec.amount;
-                }
-                    break;
-                default: break;
+            if (yearRec.is_expense) {
+                expense[yearRec.month - 1] = yearRec.amount;
+            } else {
+                gain[yearRec.month - 1] = yearRec.amount;
             }
         }
+
         for (int i = 0; i < 12; i++) {
             double profit = gain[i] - expense[i];
             System.out.println("Прибыль за " + month[i] + " составила: " + String.format("%.2f",profit) + " руб.");
--- src/MonthlyService.java
+++ src/MonthlyService.java
@@ -56,35 +56,6 @@ public class MonthlyService {
         }
     }
 
-    /*void calculationMaxProfit(int i) {
-        double maxGain = 0;
-        String maxGainItemName = null;
-        double maxExp = 0;
-        String maxExpItemName = null;
-
-        if (!(monthReports.get(i) == null)) {
-            for (MonthlyRecords monRec : monthReports.get(i).monthRecords) {
-                if (!monRec.is_expense) {
-                    if (maxGain < (monRec.quantity * monRec.sum_of_one)) {
-                        maxGain = monRec.quantity * monRec.sum_of_one;
-                        maxGainItemName = monRec.item_name;
-                    }
-                } else {
-                    if (maxExp < (monRec.quantity * monRec.sum_of_one)) {
-                        maxExp = monRec.quantity * monRec.sum_of_one;
-                        maxExpItemName = monRec.item_name;
-                    }
-                }
-            }
-            System.out.println("Название самого прибыльного товара: " + maxGainItemName + ", его сумма: " + String.format("%.2f", maxGain) + " руб.");
-            System.out.println("Самая большая трата: " + maxExpItemName + ", ее сумма: " + String.format("%.2f", maxExp) + " руб.");
-        } else if (monthReports.get(i) == null) {
-            System.out.println("Отчет отсутствует.");
-        }
-    }*/
-
-
-
     @Override
     public String toString() {
         return "MonthlyReport" + monthReports;
